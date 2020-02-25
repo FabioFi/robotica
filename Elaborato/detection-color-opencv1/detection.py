984e16b5528c7b7e0d80ed8b183e91bd0b4c5909
FabioFi <fabio.fiorio.vr@gmail.com>
2020-02-25 10:23:13 +0100

    riconosce il rosso

--- Elaborato/detection-color-opencv1/detection.py
+++ Elaborato/detection-color-opencv1/detection.py
@@ -3,7 +3,7 @@
 import cv2
 import numpy as np
 
-#Capturing Video through webcam.
+#Capturing Video through input.
 
 cap = cv2.VideoCapture('Fat_OnlyScene.mp4')
 
@@ -11,25 +11,24 @@ while(1):
         _, img = cap.read()
 
         #converting frame(img) from BGR (Blue-Green-Red) to HSV (hue-saturation-value)
-
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-        #defining the range of Yellow color
-        yellow_lower = np.array([22,60,200],np.uint8)
-        yellow_upper = np.array([60,255,255],np.uint8)
+        #defining the range of Red color
+        red_lower = np.array([161, 155, 84],np.uint8)
+        red_upper = np.array([179, 255, 255],np.uint8)
 
-        #finding the range yellow colour in the image
-        yellow = cv2.inRange(hsv, yellow_lower, yellow_upper)
+        #finding the range red colour in the image
+        red = cv2.inRange(hsv, red_lower, red_upper)
 
         #Morphological transformation, Dilation
         kernal = np.ones((5 ,5), "uint8")
 
-        blue=cv2.dilate(yellow, kernal)
+        blue=cv2.dilate(red, kernal)
 
-        res=cv2.bitwise_and(img, img, mask = yellow)
+        res=cv2.bitwise_and(img, img, mask = red)
 
-        #Tracking Colour (Yellow)
-        (_,contours,hierarchy)=cv2.findContours(yellow,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        #Tracking Colour (Red)
+        (_,contours,hierarchy)=cv2.findContours(red,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
         for pic, contour in enumerate(contours):
                 area = cv2.contourArea(contour)
@@ -40,7 +39,7 @@ while(1):
 
         cv2.imshow("Color Tracking",img)
         img = cv2.flip(img,1)
-        cv2.imshow("Yellow",res)
+        cv2.imshow("Red",res)
 
         if cv2.waitKey(10) & 0xFF == 27:
                 cap.release()
