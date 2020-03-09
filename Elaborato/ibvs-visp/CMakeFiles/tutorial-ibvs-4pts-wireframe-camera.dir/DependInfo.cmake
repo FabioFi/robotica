ea872ce856746c585651f56722ab83466d5d40d8
FabioFi <fabio.fiorio.vr@gmail.com>
2020-03-09 20:52:09 +0100

    added ibvs from visp tutorial

---
+++ Elaborato/ibvs-visp/CMakeFiles/tutorial-ibvs-4pts-wireframe-camera.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fabio/Documents/robotica/Elaborato/ibvs-visp/tutorial-ibvs-4pts-wireframe-camera.cpp" "/home/fabio/Documents/robotica/Elaborato/ibvs-visp/CMakeFiles/tutorial-ibvs-4pts-wireframe-camera.dir/tutorial-ibvs-4pts-wireframe-camera.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/x86_64-linux-gnu"
+  "/usr/include/opencv"
+  "/usr/include/eigen3"
+  "/usr/include/libxml2"
+  "/usr/include/OGRE"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- ElaboratoR2/CinematicaInversa/inv_kinematics.cpp
+++ ElaboratoR2/CinematicaInversa/inv_kinematics.cpp
@@ -12,127 +12,47 @@ using namespace std;
 
 int main(){
 
-  double d1 = 0;
-  double d4 = 0;
+  double px = 0.2;
+  double py = 0.2;
+  double pz = 0.2;
+  double ny = 0.0;
 
-  double t1 = 0.0; //0.0 1.57 -2.5 2.5
-  double t2 = 0.0; //0.0 -1.57 -2.0 2.0
-  double t3 = 0.0; //0.0 0.157 0.0 0.45
-  double t4 = 0.0; //0.0 -1.57 -3.0 3.0
+  double li = 0.3;
+  double lo = 0.3;
+  double l4 = 0.5;
+  double a1 = 0.3;
+  double a2 = 0.3;
+  double d1 = 0.4;
+  double d4 = 0.5;
 
-  double dh[4][4]={
-    0.3, 0, d1, t1,
-    0.3, 0, 0, t2,
-    0, 0, t3, 0,
-    0, PI, d4, t4,
-  };
-
-  double a1[4][4]={
-    cos(dh[0][3]), -sin(dh[0][3])*cos(dh[0][1]), sin(dh[0][3])*cos(dh[0][1]), dh[0][0]*cos(dh[0][3]),
-    sin(dh[0][3]), cos(dh[0][3]*cos(dh[0][1])), -cos(dh[0][3])*sin(dh[0][1]), dh[0][0]*sin(dh[0][3]),
-    0, sin(dh[0][1]), cos(dh[0][1]), dh[0][2],
-    0, 0, 0, 1
-  };
-
-  double a2[4][4]={
-    cos(dh[1][3]), -sin(dh[1][3])*cos(dh[1][1]), sin(dh[1][3])*cos(dh[1][1]), dh[1][0]*cos(dh[1][3]),
-    sin(dh[1][3]), cos(dh[1][3]*cos(dh[1][1])), -cos(dh[1][3])*sin(dh[1][1]), dh[1][0]*sin(dh[1][3]),
-    0, sin(dh[1][1]), cos(dh[1][1]), dh[1][2],
-    0, 0, 0, 1
-  };
-
-  double a3[4][4]={
-    cos(dh[2][3]), -sin(dh[2][3])*cos(dh[2][1]), sin(dh[2][3])*cos(dh[2][1]), dh[2][0]*cos(dh[2][3]),
-    sin(dh[2][3]), cos(dh[2][3]*cos(dh[2][1])), -cos(dh[2][3])*sin(dh[2][1]), dh[2][0]*sin(dh[2][3]),
-    0, sin(dh[2][1]), cos(dh[2][1]), dh[2][2],
-    0, 0, 0, 1
-  };
-
-  double a4[4][4]={
-    cos(dh[3][3]), -sin(dh[3][3])*cos(dh[3][1]), sin(dh[3][3])*cos(dh[3][1]), dh[3][0]*cos(dh[3][3]),
-    sin(dh[3][3]), cos(dh[3][3]*cos(dh[3][1])), -cos(dh[3][3])*sin(dh[3][1]), dh[3][0]*sin(dh[3][3]),
-    0, sin(dh[3][1]), cos(dh[3][1]), dh[3][2],
-    0, 0, 0, 1
-  };
+  double theta1 = 0;
+  double theta2 = 0;
+  double theta2bis = 0;
+  double q3 = 0;
+  double theta4 = 0;
 
   //-----------------------------------------------------------------------------
+  double temp = (px*px + py*py - a1*a1 - a2*a2)/(2*a1*a2);
+  theta2 = 1/cos(temp/*(px*px + py*py - a1*a1 - a2*a2)/(2*a1*a2)*/);
+  //theta2 = 0;
+  //theta2bis = -acos((px*px + py*py - a1*a1 - a2*a2)/(2*a1*a2));
 
-  double r1a1a2[4][4];
-  double r2r1a3[4][4];
-  double r3r2a4[4][4];
+  double s2 = sin(theta2);
+  double c2 = cos(theta2);
+  theta1 = atan2(a2*s2*px + (a1 + a2*c2)*py, (a1 + a2*c2)*px - a2*s2*py);
 
-  // Multiplying matrix a1 and a2 and storing in array r1a1a2.
-  for(int i = 0; i < 4; ++i){
-    for(int j = 0; j < 4; ++j){
-      for(int k = 0; k < 4; ++k){
-        r1a1a2[i][j] += a1[i][k] * a2[k][j];
-      }
-    }
-  }
+  q3 = d1-d4-pz;
 
-  // Multiplying matrix r1 and a3 and storing in array r2r1a3.
-  for(int i = 0; i < 4; ++i){
-    for(int j = 0; j < 4; ++j){
-      for(int k = 0; k < 4; ++k){
-        r2r1a3[i][j] += r1a1a2[i][k] * a3[k][j];
-      }
-    }
-  }
+  theta4 = -atan2(ny, 0) + theta1 - theta2;
 
-  // Multiplying matrix r2 and a4 and storing in array r3r2a4.
-  for(int i = 0; i < 4; ++i){
-    for(int j = 0; j < 4; ++j){
-      for(int k = 0; k < 4; ++k){
-        r3r2a4[i][j] += r2r1a3[i][k] * a4[k][j];
-      }
-    }
-  }
+  //cout << fixed << "temp: " << temp << endl;
+  cout << fixed << "theta1: " << theta1 << endl;
+  cout << fixed << "theta2: " << theta2 << endl;
+  //cout << fixed << "theta2bis: " << theta2bis << endl;
+  cout << fixed << "q3: " << q3 << endl;
+  cout << fixed << "theta4: " << theta4 << endl;
 
-  //----------------------------------------------------------------------------
-
-  // showing the matrix on the screen
-  cout << "a1:" << endl;
-  for(int x=0;x<4;x++){
-    for(int y=0;y<4;y++){
-        cout << fixed << a1[x][y] << " ";  // display the current element out of the array
-    }
-  cout<<endl;  // when the inner loop is done, go to a new line
-  }
-
-  // showing the matrix on the screen
-  cout << "a2:" << endl;
-  for(int x=0;x<4;x++){
-    for(int y=0;y<4;y++){
-        cout << fixed << a2[x][y] << " ";  // display the current element out of the array
-    }
-  cout<<endl;  // when the inner loop is done, go to a new line
-  }
-
-  // showing the matrix on the screen
-  cout << "a3:" << endl;
-  for(int x=0;x<4;x++){
-    for(int y=0;y<4;y++){
-        cout << fixed << a3[x][y] << " ";  // display the current element out of the array
-    }
-  cout<<endl;  // when the inner loop is done, go to a new line
-  }
-
-  cout << "a4:" << endl;
-  for(int x=0;x<4;x++){
-    for(int y=0;y<4;y++){
-        cout << fixed << a4[x][y] << " ";  // display the current element out of the array
-    }
-  cout<<endl;  // when the inner loop is done, go to a new line
-  }
-
-  // showing the matrix on the screen
-  cout << "t:" << endl;
-  for(int x=0;x<4;x++){
-    for(int y=0;y<4;y++){
-        cout << fixed << r3r2a4[x][y] << " ";  // display the current element out of the array
-    }
-  cout<<endl;  // when the inner loop is done, go to a new line
-  }
+  //printf("Theta2: %f\n", theta2);
 
   return 0;
 
